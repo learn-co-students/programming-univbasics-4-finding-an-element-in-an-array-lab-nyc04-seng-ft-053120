0009dd678b9d5fb9887013b3875b642b8f12324b
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-09 02:33:11 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,25 @@
+#def find_element_index(array, value_to_find)
+#  count = 0
+#  found_value_index = nil
+#  while count < array.length do
+#    if array[count] == value_to_find
+#      found_value_index = count
+#    end
+#    count += 1
+#  end
+#  found_value_index
+#end
+
+#def find_element_index(array, value_to_find)
+#  index = 0
+#  while index < array.length do
+#    if array[index] == value_to_find
+#      return index
+#    end
+#    index += 1
+#  end
+#end
+
 def find_element_index(array, value_to_find)
-  index = 0
-  while index < array.length do
-    if array[index] == value_to_find
-      return index
-    end
-    index += 1
-  end
+  array.index(value_to_find)
 end
